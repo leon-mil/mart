739324f92776196c3bf7571011ce97e623ee216e
Leon Mil <leon.mil@gmail.com>
2022-10-14 00:17:06 -0400

    added ping1-4 fileExist

--- library/utilities/network/diagnostic.py
+++ library/utilities/network/diagnostic.py
@@ -1,8 +1,12 @@
 #! /usr/local/bin/python3
 
-import platform    # For getting the operating system name
+from asyncio.subprocess import DEVNULL, PIPE
+from logging import captureWarnings
+import platform
+from stat import filemode    # For getting the operating system name
 import subprocess  # For executing a shell command
 import os
+from sys import stderr
 
 """
       Programer: Leon Mil
@@ -14,39 +18,100 @@ def ping(host):
     
     # Option for the number of packets as a function of
     param = '-n' if platform.system().lower()=='windows' else '-c'
+    
+    #ICMP Echo Number
+    icmp_num = '1' 
 
     # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
-
-    return subprocess.call(command) == 0
+    command = ['ping', param, icmp_num, host]
+    
+    with open('logs/network/diagnostic.log', 'w') as file_log:
+        result = subprocess.call(
+            command,
+            stdout=file_log,
+            stderr=file_log)
+    
+    return ( result == 0 )
 
 def ping2(host):
-    icmp_num = 1 #ICMP Echo Number
-    os.system('ping -q -c {} {}'.format(icmp_num, host))
-
-def ping3(host):
     
      # Option for the number of packets as a function of
     param = '-n' if platform.system().lower()=='windows' else '-c'
     
+    #ICMP Echo Number
+    icmp_num = '1' 
+    
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, icmp_num, host]
+    
     try:
         response = subprocess.check_output(
-            ['ping', '-c', '1', host],
-            stderr=subprocess.STDOUT,  # get all output
-            universal_newlines=True  # return string not bytes
+            command,
+            stderr = subprocess.STDOUT, # get all output
+            universal_newlines = True   # return string not bytes
         )
     except subprocess.CalledProcessError:
         response = None
+        
+    return response
 
-def ping4(host):
+def ping3(host):
+    
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    
+    #ICMP Echo Number
+    icmp_num = '1' 
+    
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, icmp_num, host]
+    
     with open(os.devnull, 'w') as DEVNULL:
         try:
             result = subprocess.check_call(
-                ['ping', '-c', '1', host],
+                command,
                 stdout=DEVNULL,  # suppress output
                 stderr=DEVNULL
             )
-            is_up = True
-            #return (result == 0)
-        except subprocess.CalledProcessError:
-            is_up = False
+            is_up = (result == 0)
+        except subprocess.CalledProcessError as e:
+            is_up = (e.returncode == 0)
+        
+        return is_up
+    
+def ping4(host):
+    
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    #ICMP Echo Number
+    icmp_num = '1' 
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, icmp_num, host]
+      
+    with open('logs/hosts.log', 'a') as file_log: 
+        # p1 = subprocess.run(command, capture_output=True, text=True)
+        # p2 = subprocess.run(command, stdout=PIPE, text=True)
+        p3 = subprocess.run(command, stdout=file_log, text=True)
+        #print(p3.stdout)
+        
+def fileExists(filename):
+    
+    with open('logs/files.log', 'a') as file_log: 
+        command = ['ls', '-ltr', filename]
+        
+        try:
+            result = subprocess.run(
+                command, 
+                capture_output=True,    # If capture_output is true, stdout and stderr will be captured.
+                text=True,              # If text mode is not used, stdin, stdout and stderr will be opened as binary streams. We will need to use.decode().
+                check=True,             # If returncode is non-zero, raise a CalledProcessError.
+                stdout=file_log
+            )
+        except subprocess.CalledProcessError as e:
+            print(e.stderr)
+    
+    return (result.returncode == 0)
+    
+    
--- main.py
+++ main.py
@@ -7,10 +7,18 @@ from library.definitions.network.domain import *
 
 hosts = [steps21, steps23, steps28]
 
+# Clear log
+with open('logs/hosts.log', 'w'): pass
+
 results = {}
 for host in hosts: 
     results[host] = 'Destination Host Reachable' if ping4(host) else 'Destination Host Unreachable' 
-        
+
+filename = 'logs/hosts2.log'
+p1 = fileExists(filename)
+print('STDERR: \n' + 'No Errors \n' if (len(p1.stderr)==0) else p1.stderr + '\n' )
+print('STDOUT: \n' + p1.stdout)
+       
 #print1(*hosts, title = "Host")
-print2(**results)
+#print2(**results)
 
