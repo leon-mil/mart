c3ee80891edb508d85b704b7cd09c38a357b30e3
Leon Mil <leon.mil@gmail.com>
2022-10-15 11:10:58 -0400

    stuff

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ from library.definitions.network.domain import *
 
 hosts = [steps21, steps23, steps28]
 
-# Clear log
+# Clear host log
 with open('logs/hosts.log', 'w'): pass
 
 results = {}
 for host in hosts: 
     results[host] = 'Destination Host Reachable' if ping4(host) else 'Destination Host Unreachable' 
 
-filename = 'logs/hosts2.log'
+# Clear host log
+with open('logs/files.log', 'w'): pass
+
+filename = 'logs/files2.log'
 p1 = fileExists(filename)
-print('STDERR: \n' + 'No Errors \n' if (len(p1.stderr)==0) else p1.stderr + '\n' )
-print('STDOUT: \n' + p1.stdout)
+
+# print('STDERR: \n' + 'No Errors \n' if (len(p1.stderr)==0) else p1.stderr + '\n' )
+# print('STDOUT: \n' + p1.stdout)
        
-#print1(*hosts, title = "Host")
-#print2(**results)
+
 
